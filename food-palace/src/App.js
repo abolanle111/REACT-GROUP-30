adac82fa9e25298b21bba08b8d9ee91656a29d32
Ekunola Paul Oluwapelumi <pscon@Ekunolas-MacBook-Pro.local>
2022-04-22 09:13:16 +0100

    adding navbar and routes

--- food-palace/src/App.js
+++ food-palace/src/App.js
@@ -1,8 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
+import Contact  from "./components/Contact"
+import Products from "./components/Products"
+import About  from "./components/About"
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+// import User from "./pages/User/User";
 
 function App() {
   return (
-      <div>hello</div>
+    <BrowserRouter>
+<Nav/>
+      {/* <Home/> */}
+      <Routes>
+      <Route path="/" exact index element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="products" element={<Products/>} />
+      <Route path="about" element={<About />} />
+      </Routes>
+
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ food-palace/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Nav.css"
+
+function Nav() {
+  return (
+    
+    <div className="HeaderGroup">
+    <Link to="/#">
+
+      <h2 className="palaceTitle">Food Palace</h2>
+    </Link>
+      <Link to="/">Home</Link>
+      <Link to="/products">Products</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    
+    </div>
+ 
+  )
+}
+
+export default Nav
